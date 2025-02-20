3e3c9c654f6eeef7884d9445077e4b74936be97c
jordan-mace <jordz98@gmail.com>
2025-02-20 19:35:21 +1100

    Add video file to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-        <div>
-          <p>Hi there 👋 I&apos;m Jordan</p>
-          <p>I&apos;m a frontend developer from Melbourne, Australia</p>
-          <p style={{ marginTop: '1rem', textAlign: 'center'}}>This website is 🚧 under construction 🚧</p>
-        </div>
+    <div>
+      <video autoPlay loop muted className={styles.backgroundVideo}>
+        <source src="/2818546-hd_1280_720_24fps.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className={styles.content}>
+      <p>Hi there 👋 I&apos;m Jordan</p>
+      <p>I&apos;m a frontend developer from Melbourne, Australia</p>
+      <p style={{ marginTop: '1rem', textAlign: 'center' }}>This website is 🚧 under construction 🚧</p>
+      </div>
+    </div>
   );
 }
